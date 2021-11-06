bd83dee2922f92aad70e29b7787b9be597cd3bec
Haneen Hashlamoun <haneen.hashlamoun@gmail.com>
2021-11-06 19:48:54 +0200

    cover tests

--- tests/test_caesar_cipher.py
+++ tests/test_caesar_cipher.py
@@ -27,3 +27,42 @@ def test_crack():
     actual = crack(word)
     expected = 'nope10'
     assert actual == expected
+
+
+def test_encrypt_shift_1():
+    actual = encrypt("apple", 1)
+    expected = "bqqmf"
+    assert actual == expected
+
+def test_encrypt_shift_10():
+    actual = encrypt("apple", 10)
+    expected = "kzzvo"
+    assert actual == expected    
+
+def test_uppercase():
+    actual = encrypt("BANANA", 10)
+    expected = "LKXKXK"
+    assert actual == expected
+
+def test_with_whitespace():
+    actual = encrypt("apples and bananas", 1)
+    expected = "bqqmft boe cbobobt"
+    assert actual == expected    
+
+def test_round_trip():
+    original = "Gimme a 1!"
+    shift = 5
+    encrypted = encrypt(original, shift)
+    actual = decrypt(encrypted, shift)
+    expected = original
+    assert actual == expected        
+
+def test_decrypt_with_whitespace():
+    actual = decrypt("bqqmft boe cbobobt", 1)
+    expected = "apples and bananas"
+    assert actual == expected     
+
+def test_decrypt_uppercase():
+    actual = decrypt("LKXKXK", 10)
+    expected = "BANANA"
+    assert actual == expected  
